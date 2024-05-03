879908c891a178e0c57b3d825f1526aab2765d27
ezequielpsm <79644416+ezequielpsm@users.noreply.github.com>
2024-05-03 19:52:10 -0300

    Add files via upload

--- Trabalho-front/Telas-do-projeto/agendamentos_e_reunioes.js
+++ Trabalho-front/Telas-do-projeto/agendamentos_e_reunioes.js
@@ -1,22 +1,71 @@
-document.getElementById("agendarForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    var nomeReuniao = document.getElementById("nome").value;
-    var horarioReuniao = document.getElementById("horario").value;
-    var dataReuniao = document.getElementById("data").value;
+document.getElementById("agendarForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  var nomeReuniao = document.getElementById("nome").value;
+  var horarioReuniao = document.getElementById("hora").value;
+  var dataReuniao = document.getElementById("data").value;
+  var email = document.getElementById("emails").value;
   
+
+
+  var novoAgendamento = document.createElement("ul");
+  novoAgendamento.textContent = "Nome da Reunião (Treinamento): " + nomeReuniao + " - Horário: " + horarioReuniao + " - Data: " + dataReuniao + " \nParticipantes: " + email + ", " + "."  ;
+
+
+  var listaAgendamentos = document.getElementById("listaAgendamentos");
+  listaAgendamentos.appendChild(novoAgendamento);
+
+
+  // document.getElementById("nome").value = "";
+  // document.getElementById("hora").value = "";
+  // document.getElementById("data").value = "";
+  // document.getElementById("emails").value = "";
+
+
+  // document.getElementById("confirmacao").style.display = "block";
+
+});
+
+const formEl = document.getElementById('agendarForm');
+const respostaEl = document.getElementById('resposta-api');
+
+formEl.addEventListener('submit', evento => {
+  evento.preventDefault();
+
+  const formData = new FormData(formEl);
+  const data = {
+    nome: formData.get('nome'),
+    data: formData.get('data'),
+    hora: formData.get('hora'),
+    emails: formData.get('emails'),
     
-    var novoAgendamento = document.createElement("li");
-    novoAgendamento.textContent = "Nome da Reunião (Treinamento): " + nomeReuniao + " - Horário: " + horarioReuniao + " - Data: " + dataReuniao;
-  
-   
-    var listaAgendamentos = document.getElementById("listaAgendamentos");
-    listaAgendamentos.appendChild(novoAgendamento);
-  
-    
-    document.getElementById("nome").value = "";
-    document.getElementById("horario").value = "";
-  
-    
-    document.getElementById("confirmacao").style.display = "block";
+  };
+
+  fetch("http://172.16.98.143:8000/core/create-meeting/", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(res => {
+    if (!res.ok) {
+      return res.json().then(error => Promise.reject(error));
+    }
+    return res.json();
+  }).then(data => {
+    if (data.msg) {
+      respostaEl.textContent = data.msg;
+
+      //window.location.href = "tela_de_login.html"; 
+    } else {
+      respostaEl.textContent = 'Resposta inválida recebida da API';
+    }
+  }).catch(error => {
+    console.error('Erro:', error);
+    if (error.msg) {
+      respostaEl.textContent = error.msg;
+    } else {
+      respostaEl.textContent = 'Erro ao enviar para a API';
+    }
   });
-  
+});
+
--- Trabalho-front/Telas-do-projeto/cadastro-de-Funcionario.js
+++ Trabalho-front/Telas-do-projeto/cadastro-de-Funcionario.js
@@ -1,35 +1,42 @@
 const formEl = document.getElementById('form-api');
-        const respostaEl = document.getElementById('resposta-api');
+const respostaEl = document.getElementById('resposta-api');
 
-        formEl.addEventListener('submit', evento => {
-            evento.preventDefault();
+formEl.addEventListener('submit', evento => {
+    evento.preventDefault();
 
-            const formData = new FormData(formEl);
-            const data = {
-                usuario: formData.get('usuario'),
-                nome: formData.get('nome'),
-                email: formData.get('email'),
-                senha: formData.get('senha')
-            };
+    const formData = new FormData(formEl);
+    const data = {
+        usuario: formData.get('usuario'),
+        nome: formData.get('nome'),
+        email: formData.get('email'),
+        senha: formData.get('senha')
+    };
+
+    fetch("http://172.16.98.143:8000/core/create-user/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => {
+        if (!res.ok) {
+            return res.json().then(error => Promise.reject(error));
+        }
+        return res.json();
+    }).then(data => {
+        if (data.msg) {
+            respostaEl.textContent = data.msg;
             
-            fetch("http://172.16.98.143:8000/core/create-user/", {
-                method: 'POST', 
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            }).then(res => res.json())
-              .then(data => {
-                  if (data.msg) {
-                      respostaEl.textContent = data.msg;
-                      // Redirecionamento para a página de login após o sucesso do cadastro
-                      window.location.href = "tela_de_login.html"; // Altere para o caminho correto da sua página de login
-                  } else {
-                      respostaEl.textContent = 'Resposta inválida recebida da API';
-                  }
-              })
-              .catch(error => {
-                  console.error('Erro:', error);
-                  respostaEl.textContent = 'Erro ao enviar requisição para a API';
-              });
-        });
+            window.location.href = "tela_de_login.html"; 
+        } else {
+            respostaEl.textContent = 'Resposta inválida recebida da API';
+        }
+    }).catch(error => {
+        console.error('Erro:', error);
+        if (error.msg) {
+            respostaEl.textContent = error.msg;
+        } else {
+            respostaEl.textContent = 'Erro ao enviar requisição para a API';
+        }
+    });
+});
