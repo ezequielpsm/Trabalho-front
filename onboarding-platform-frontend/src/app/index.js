15ad1109b3e77a3346e3e50749ff4c67396eb5ff
ezequielpsm <79644416+ezequielpsm@users.noreply.github.com>
2024-08-13 20:02:33 -0300

    Delete onboarding-platform-frontend directory

--- onboarding-platform-frontend/src/app/index.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('scrollToContact').addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector('.contato').scrollIntoView({ behavior: 'smooth' });
-});
